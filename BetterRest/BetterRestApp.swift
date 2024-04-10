49661c63980dd89a162f43afe020779396de2164
Edwin Peraza <98931477+edwinperaza99@users.noreply.github.com>
2024-04-09 22:58:28 -0700

    Initial commit: Adding project files

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by csuftitan on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
